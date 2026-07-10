d38d02086b4d975db8b8fcc9fc14db539610620a
gouthamraj112 <goutham.raj@prodapt.com>
2026-07-10 22:57:24 +1300

    Add files via upload

---
+++ Day5/Welcome.py
@@ -0,0 +1,18 @@
+print("Welcome to python VS Code")
+
+x=1
+print(x+1)
+print(eval("x+1"))
+a=5
+b=5
+print(a==b)
+print(a is b)
+print(id(a))
+
+expression="10*20"
+print(expression)
+print(eval(expression))
+
+expression=input("Enter your expression: ")
+result=eval(expression)
+print("The result of the expression is: ", result)
---
+++ Day5/membership.py
@@ -0,0 +1,5 @@
+language="Python"
+print("Python" in language)
+print("java" in language)
+
+print(3 in [1,2,3])
