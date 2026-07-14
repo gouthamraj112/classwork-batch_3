603305dbb7ae6e23c35b08ae89f7536215c7d320
gouthamraj112 <goutham.raj@prodapt.com>
2026-07-14 22:27:14 +1300

    Add files via upload

---
+++ day-7/file1.py
@@ -0,0 +1,18 @@
+file = open('Day_7/students.txt', 'r')
+
+data = file.read()
+print(data)
+file.close()
+
+file = open('Day_7/students.txt', 'w+')
+file.write("this is a new line of text.\n")
+file.write("this is another new line of text.\n")
+
+file.seek(0)  # Move the cursor to the beginning of the file
+print(file.read())
+file.close()
+
+# file = open('Day_7/students.txt', 'r')
+# x = file.read()
+# print(x)
+# file.close()
